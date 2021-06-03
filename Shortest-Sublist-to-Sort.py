4bb0b4bfc0c2dcd2ebe9f2e315575278ca443755
Fatima Jawed <55167453+fzjawed@users.noreply.github.com>
2021-06-03 14:44:46 +0300

    Add files via upload

---
+++ Shortest-Sublist-to-Sort.py
@@ -0,0 +1,22 @@
+class Solution:
+    def solve(self, nums):
+        if len(nums) < 2:
+            return 0
+        prev = nums[0]
+        end = 0
+        for i in range(len(nums)):
+            if nums[i] < prev:
+                end = i
+            else:
+                prev = nums[i]
+        start = len(nums)-1
+        prev = nums[start]
+        for i in range(len(nums)-1,-1,-1):
+            if prev < nums[i]:
+                start = i
+            else:
+                prev = nums[i]
+        if end != 0:
+            return end - start + 1
+        else:
+            return 0
